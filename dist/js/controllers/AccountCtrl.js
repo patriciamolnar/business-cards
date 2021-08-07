81632bab01b20eb7130e50882fb86658b6c7161c
Patricia Molnar <m.patricia@live.com>
2021-08-07 14:10:28 +0100

    adding option to update firstname, lastname, email in DB table `users`

--- dist/js/controllers/AccountCtrl.js
+++ dist/js/controllers/AccountCtrl.js
@@ -1,6 +1,6 @@
 const AccountCtrl = angular.module('AccountCtrl', []); 
 
-AccountCtrl.controller('AccountCtrl', function($scope, $rootScope, validationService) {
+AccountCtrl.controller('AccountCtrl', function($scope, $rootScope, validationService, $http) {
   $scope.regex = {
     email: validationService.getEmailRegex(),
     string: validationService.getStringRegex(),
@@ -41,19 +41,61 @@ AccountCtrl.controller('AccountCtrl', function($scope, $rootScope, validationSer
   const u = $rootScope.$storage.user;
 
   $scope.user = {
-    firstname: u.firstname ?? '', 
-    lastname: u.lastname ?? '', 
-    jobtitle: u.jobtitle ?? '',
-    description: u.description ?? '',
-    sector: u.sector ?? '',
-    office: u.office ?? '', 
-    mobile: u.mobile ?? '', 
-    email: u.email ?? '',  
-    website: u.website ?? '',  
-    social: {
+    core: { //fields for `users` mySQL table
+      firstname: u.firstname ?? '', 
+      lastname: u.lastname ?? '', 
+      email: u.email ?? '',
+    },
+    add: { //fields for `details` mySQL table
+      jobtitle: u.jobtitle ?? '',
+      description: u.description ?? '',
+      sector: u.sector ?? '',
+      office: u.office ?? '', 
+      mobile: u.mobile ?? '',   
+      website: u.website ?? '',
       twitter: u.twitter ?? '', 
       instagram: u.instagram ?? '', 
       facebook: u.facebook ?? ''
+    }, 
+  }
+
+  $scope.password = ''; 
+
+  $scope.result = null; 
+  $scope.errors = []; 
+
+  $scope.updateAccount = function() {
+    //check if password has been provided.
+    if(!$scope.password.trim()) {
+      $scope.errors.push('Please provide your password to save the changes'); 
+      return; 
+    }
+  
+    //check if any of the core values have changed. 
+    const coreDetails = {};
+    for (const [key, value] of Object.entries($scope.user.core)) {
+      if(value !== u[key]) {
+        coreDetails[key] = value;
+      }
+    }
+
+    if(Object.keys(coreDetails).length !== 0) { //details have changed, update DB
+      $http({
+        url: 'php/includes/update-account.inc.php',
+        method: 'POST', 
+        data: {
+          originalEmail: u.email, //for account validation send original email and passw.
+          password: $scope.password, 
+          core: coreDetails
+        }
+      })
+      .then(function(response) {
+        console.log(response.data);
+      })
+      .catch(function(error) {
+        console.log(error);  
+        throw error;
+      })
     }
   }
 }); 
---
+++ dist/php/includes/update-account.inc.php
@@ -0,0 +1,101 @@
+<?php 
+require_once '../functions.php'; 
+require_once './connect.inc.php'; 
+
+if(!request_is_post()) {
+  echo json_encode([
+    'success' => false, 
+    'error' => 'Received request has incorrect format.'
+  ]);
+  exit(); 
+} else {
+  $postJSON = file_get_contents("php://input");
+  $dataArr = json_decode($postJSON, TRUE); 
+
+  $firstname = $dataArr['core']['firstname'] ?? null;
+  $lastname = $dataArr['core']['lastname'] ?? null;
+  $originalEmail = $dataArr['originalEmail'] ?? null; 
+  $newEmail = $dataArr['core']['email'] ?? null; 
+  $password = $dataArr['password'] ?? null; 
+
+  //check if original email from localstorage was sent on
+  if(empty(trim($originalEmail))) { 
+    echo json_encode([
+      'success' => false, 
+      'errors' => 'blank'
+    ]);
+    exit(); 
+  }
+
+  //check if there is a user with email
+  $user = get_account_info('*', 'email', $originalEmail);
+  if(!$user) {
+    echo json_encode([
+      'success' => false, 
+      'errors' => 'incorrect'
+    ]);
+
+    exit(); 
+  } 
+
+  //verify password correct
+  $password_check = password_verify($password, $user['password']);
+  if(!$password_check) {
+    echo json_encode([
+      'success' => false, 
+      'errors' => 'incorrect'
+    ]);
+    exit();
+  } else { //update DB
+    $q = array();
+    if(!empty(trim($firstname))) {
+      $q[] = "firstname = :firstname";
+    }
+    if(!empty(trim($lastname))) {
+      $q[] = "lastname = :lastname";
+    }
+    if(!empty(trim($newEmail))) {
+      $q[] = "email = :newEmail";
+    }
+    if(sizeof($q) > 0) { //check if we have any updates otherwise don't execute
+        $sql = "UPDATE users SET " . implode(", ", $q) . " WHERE email= :originalEmail";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(":originalEmail", $originalEmail);
+
+        if(!empty(trim($firstname))) {
+          $stmt->bindParam(":firstname", $firstname);
+        }
+        if(!empty(trim($lastname))) {
+          $stmt->bindParam(":lastname", $lastname);
+        }
+        if(!empty(trim($newEmail))) {
+          $stmt->bindParam(":newEmail", $newEmail);
+        }
+
+        $success = $stmt->execute();
+        // echo json_encode($q);
+
+        if($success) { //update success: return userdata to frontend for localstorage
+          echo json_encode([
+            'success' => true, 
+            'errors' => '',
+            'user' => array(
+              'firstname' => $firstname ?? $user['firstname'],
+              'lastname' => $lastname ?? $user['lastname'], 
+              'email' => $newEmail ?? $user['email']
+            )
+          ]);
+        } else { // return error
+          echo json_encode([
+            'success' => false, 
+            'errors' => 'sql'
+          ]); 
+        }
+    } else {
+      echo json_encode([
+        'success' => false, 
+        'errors' => 'nochange'
+      ]);
+    }
+  }
+}
