70a95700609ebc1ab1d61d79bf07f4066cd50478
Patricia Molnar <m.patricia@live.com>
2021-08-11 16:56:51 +0100

    changing button text from 'save contact'/'unsave contact' based on saved status

--- dist/js/controllers/ProfileCtrl.js
+++ dist/js/controllers/ProfileCtrl.js
@@ -1,6 +1,7 @@
 const ProfileCtrl = angular.module('ProfileCtrl', []);
 
 ProfileCtrl.controller('ProfileCtrl', function($scope, $http, $routeParams, $localStorage) {
+  $scope.btnMessage = 'Save Contact'
   $http({
     url: 'php/includes/user.inc.php?id=' + $routeParams.id,
     method: 'GET'
@@ -28,10 +29,22 @@ ProfileCtrl.controller('ProfileCtrl', function($scope, $http, $routeParams, $loc
       }
     })
     .then(function(response) {
-      console.log(response);
+      if(response.data.success === true) {
+        if(response.data.message === 'saved') {
+          $scope.btnMessage = 'Unsave Contact';
+        }
+
+        if(response.data.message === 'unsaved') {
+          $scope.btnMessage = 'Save Contact';
+        }
+      } else {
+        $scope.error = response.data.error; 
+      }
     })
     .catch(function(error) {
+      $scope.error = error;
       console.log(error); 
+      throw error; 
     })
   }
 });
