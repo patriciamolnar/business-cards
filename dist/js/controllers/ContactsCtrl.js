e21e26dda92ca0fe69c3fa171961998e130df70e
Patricia Molnar <m.patricia@live.com>
2021-08-18 11:33:08 +0100

    simplifying ContactsCtrl with handleResponse service

--- dist/js/controllers/ContactsCtrl.js
+++ dist/js/controllers/ContactsCtrl.js
@@ -1,19 +1,9 @@
 const ContactsCtrl = angular.module('ContactsCtrl', []); 
 
-ContactsCtrl.controller('ContactsCtrl', function($scope, $http, $localStorage) {
+ContactsCtrl.controller('ContactsCtrl', function($scope, $http, $localStorage, handleResponse) {
   //get all contacts from DB
   $http.get('php/includes/get-contacts.inc.php?id=' + $localStorage.user.id)
-    .then(function(response) {
-      if(response.data.success === true) {
-        $scope.contacts = response.data.contacts; 
-        $scope.error = ''; 
-      } else {
-        $scope.error = response.data.error; 
-      }
-    })
-    .catch(function(error) {
-      console.log(error); 
-      $scope.error = error; 
-    });
+    .then(response => handleResponse.handleResponse($scope, response, 'contacts'))
+    .catch(error => console.log(error));
 });
 
